6c3dc8d8580fe0c7a836538b892fbd82f31757ba
Ahmad Qamar <ahmadqamar.rq@gmail.com>
2026-05-02 22:41:03 +0500

    .

--- a_main/b_app/models.py
+++ a_main/b_app/models.py
@@ -3,29 +3,28 @@ from django.db import models
 # Create your models here.
 
 class Departmet(models.Model):
-    name=models.CharField(max_length=100)
+    dep_name=models.CharField(max_length=100)
     def __str__(self):
-        return self.name
-    
+        return self.dep_name    
 
 
 class Section(models.Model):
-    name=models.CharField(max_length=100)
+    sec_name=models.CharField(max_length=100)
     department=models.ForeignKey(Departmet,on_delete=models.CASCADE)
     def __str__(self):
-        return  self.name
+        return  self.sec_name
     
 
 class Student(models.Model):
     departmrct=models.ForeignKey(Departmet,on_delete=models.CASCADE)
     section=models.ForeignKey(Section,on_delete=models.CASCADE)
-    name=models.CharField(max_length=100)
-    age=models.IntegerField()
-    email=models.EmailField(unique=True)
+    stu_name=models.CharField(max_length=100)
+    stu_age=models.IntegerField()
+    stu_email=models.EmailField(unique=True)
     roll=models.IntegerField(max_length=3)
 
     def __str__(self):
-        return self.name
+        return self.stu_name
 
 
 
--- a_main/b_app/views.py
+++ a_main/b_app/views.py
@@ -1,8 +1,22 @@
 from django.shortcuts import render
+from .models import *
 
 # Create your views here.
 
 
 
 def home_page(request):
+    if request.method=='POST':
+        data=request.POST
+        dep_name=data.get('dep_name')
+
+
+
+
+
+        Departmet.objects.create(
+        dep_name=dep_name,
+
+        )
+
     return render(request,'home.html')
